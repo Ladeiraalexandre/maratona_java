a592ac1d7d0d9fad4463793d9ca87491884f4973
Ladeiraalexandre <ladeiraalexandre@gmail.com>
2024-07-31 15:29:19 -0300

    Iniciando repositorio aula 01

---
+++ src/edu/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,10 @@
+package edu.maratonajava.introducao;
+
+public class Aula01OlaDevDojo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
